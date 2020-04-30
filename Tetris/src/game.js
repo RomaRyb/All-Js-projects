6be4a6eb471e24356226c32c4e844b1d0a0ba556
Roma Rub <rubashevskyiroman@gmail.com>
2020-04-30 20:52:14 +0300

    added collision with other tetrominos and oth

--- Tetris/src/game.js
+++ Tetris/src/game.js
@@ -37,21 +37,60 @@ export default class Game {
 
     movePieceLeft() {
         this.activePiece.x -= 1;
+
+        if (this.hasCollision()){        //check if we are out of field, and if its  so? than return figure on place before 
+            this.activePiece.x += 1;
+        }
     }
 
     movePieceRight() {
         this.activePiece.x += 1;
+
+        if (this.hasCollision()){        //check if we are out of field, and if its  so? than return figure on place before 
+            this.activePiece.x -= 1;
+        }
     }
 
     movePieceDown() {
         this.activePiece.y += 1;
+
+        if (this.hasCollision()){        //check if we are out of field, and if its  so? than return figure on place before 
+            this.activePiece.y -= 1;
+
+            this.lockPiece();
+        }
     }
 
     //check if tetromino isn't out of field
-    isPieceOutOfBounds(){
-        const {}        //деструктуризація
-        return this.playfield[this.activePiece.y] === undefined || this.playfield[this.activePiece.y][this.activePiece.x] === undefined;
+    hasCollision(){
+        const {y: pieceY, x: pieceX, blocks} = this.activePiece;                                            
 
+        for (let y = 0; y < blocks.length; y++){                                                            
+            for (let x = 0; x < blocks[y].length; x++) {     
+                if(
+                    blocks[y][x] &&
+                    ((this.playfield[pieceY + y] === undefined || this.playfield[pieceY + y][pieceX + x] === undefined) ||
+                    this.playfield[pieceY + y][pieceX + x])
+                ) {
+                    return true;
+                }                                                                                                                                 
+            }
+        }
+
+        return false;
+    }
+
+    //додаємо метод, який дозволяє перенести значення з масиву активної фігури(activePice) в ігрове поле
+    lockPiece() {
+        const {y: pieceY, x: pieceX, blocks} = this.activePiece;                                            //деструктуризація
+
+        for (let y = 0; y < blocks.length; y++){                                                            //зовнішній цикл перебирає ряди activePiece.blocks
+            for (let x = 0; x < blocks[y].length; x++) {                                                    //внутрішній перебирає елементи кожного ряду
+                if (blocks[y][x]) {
+                     this.playfield[pieceY + y][pieceX + x] = blocks[y][x];                                      //копіюєм знчення з масива blocks в  playfield
+                }
+            }
+        }
     }
 }
 
