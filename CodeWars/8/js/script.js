fb8a2d90c13ee215920e1613c993e2b2c3a348b6
Roma Rub <rubashevskyiroman@gmail.com>
2020-05-06 23:46:00 +0300

    kata 9

--- CodeWars/8/js/script.js
+++ CodeWars/8/js/script.js
@@ -8,6 +8,8 @@
 
 // Input: 123456789 Output: 987654321
 
+//https://medium.com/@stasonmars/%D0%BA%D0%B0%D0%BA-%D0%B4%D0%B5%D0%BB%D0%B0%D1%82%D1%8C-%D1%81%D0%BE%D1%80%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%BA%D1%83-%D0%B2-javascript-%D0%BF%D1%80%D0%B8-%D0%BF%D0%BE%D0%BC%D0%BE%D1%89%D0%B8-sort-d492c97ccfdf
+
 //===================================== 1 =====================================
 
 
