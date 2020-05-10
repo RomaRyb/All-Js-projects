f11887b1e0234041c1aa2f81a15ca835754d4ffb
Roma Rub <rubashevskyiroman@gmail.com>
2020-05-11 00:17:21 +0300

    last 2 soll of kata 2

--- CodeWars/12/js/script.js
+++ CodeWars/12/js/script.js
@@ -33,22 +33,22 @@
 //     return s == arr.reverse().join("");
 // }
 //======================================================
-longestPalindrome = function (s){
-    let minLength = 0;
+// longestPalindrome = function (s){
+//     let minLength = 0;
 
-    for (let i = 0; i < s.length; i++){
-        for (let j = i + 1; j <= s.length; j++){
-            let str = s.slice(i,j);
-            if (isPalindrome(str) && minLength < s.length){
-                minLength = s.length;
-            }
-        }
-    }
-    return minLength;
-}
-function isPalindrome(s) {
-    return palindrome = s.split('').reverse().join('');
-}
+//     for (let i = 0; i < s.length; i++){
+//         for (let j = i + 1; j <= s.length; j++){
+//             let str = s.slice(i,j);
+//             if (isPalindrome(str) && minLength < s.length){
+//                 minLength = s.length;
+//             }
+//         }
+//     }
+//     return minLength;
+// }
+// function isPalindrome(s) {
+//     return palindrome = s.split('').reverse().join('');
+// }
 
 
 
@@ -68,3 +68,53 @@ function isPalindrome(s) {
 //     }
 //     return true;
 //}
+
+// function longestPalindrome(s) {
+//     if (s === '') {
+//         return '';
+//     }
+//     let arr = [];
+//     let _s = s.split('');
+//     for (let i = 0; i < _s.length; i++) {
+//         for (let j = 0; j < _s.length; j++) {
+//             let word = _s.slice(0, j + 1).join('');
+//             let rev_word = _s.slice(0, j + 1).reverse().join('');
+//             if (word === rev_word) {
+//                 arr.push(word)
+//             }
+//         }
+//         _s.splice(0, 1)
+//     }
+//     let _arr = arr.sort((a, b) => a.length - b.length);
+//     for (let i = 0; i < _arr.length; i++) {
+//         if (_arr[arr.length - 1].length === _arr[i].length) {
+//             return _arr[i]
+//         }
+//     }
+// }
+
+
+// ========================= sol was so easy lol ==============================
+var longestPalindrome = function (s) {
+    if (!s) return 0;
+    for (let c = s.length; c > 0; c--) {
+        for (let i = 0; i <= s.length - c; i++) {
+            var check = s.substr(i, c);
+            if (check === check.split("").reverse().join("")) return c;
+        }
+    }
+}
+
+// ==================== another sol =============================
+longestPalindrome = function (s) {
+    //your code here
+    let isPalindrome = str => str.split('').reverse().join('') === str;
+
+    for (let i = s.length; i >= 0; i--) {
+        for (let j = s.length - i; j >= 0; j--) {
+            if (isPalindrome(s.substr(j, i))) {
+                return i;
+            }
+        }
+    }
+}
